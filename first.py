776ccf22dd96038e62aa1f779d7c5834422e91e6
Anupam Paul <ajp4921@gmail.com>
2022-11-15 00:38:44 +0600

    first commit

---
+++ first.py
@@ -0,0 +1,25 @@
+# print("anupam")
+
+# person = 'Anupam'
+
+# age = 25
+
+# about_me = f'My name is {person}. And my age is {age}.'
+# print(about_me)
+
+# print(type(person))
+# print(type(age))
+
+num1 = input('Give first number : ')
+num2 = input('Give second number : ')
+
+num1_int = int(num1)
+num2_int = int (num2)
+
+result = num1_int + num2_int
+
+# print('Result is : ',result)
+# x="Learning"
+# y="Python"
+# z=x+y
+# print(z)
